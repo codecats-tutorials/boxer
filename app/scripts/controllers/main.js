1f4c22a484af347c3b98755131221b9f79d33e03
ssstrz <codecats@gmail.com>
2015-03-28 00:31:29 +0100

    main page animations on viewport

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,37 +9,33 @@
  */
 angular.module('projApp')
   .controller('MainCtrl', function ($scope, $timeout) {
+    function checkVisible (elm, evalu) {
+        evalu = evalu || "visible";
+        var vpH = angular.element(window).height(), // Viewport Height
+            st = angular.element(window).scrollTop(), // Scroll Top
+            y = angular.element(elm).offset().top,
+            elementHeight = angular.element(elm).height();
 
-function isScrolledIntoView(elem) {
-    var $elem = $(elem);
-    var $window = $(window);
-
-    var docViewTop = $window.scrollTop();
-    var docViewBottom = docViewTop + $window.height();
-
-    var elemTop = $elem.offset().top;
-    var elemBottom = elemTop + $elem.height();
-    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-$(window).on('DOMContentLoaded load resize scroll', function () {
-      var sec1 = $('#sec-1');
-  setInterval(function () {
-    console.log('a')
-    if (isScrolledIntoView('#sec-1')) {
-      alert('a')
+        if (evalu == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
+        if (evalu == "above") return ((y < (vpH + st)));
+        if (evalu == "past") return ((y + elementHeight < (vpH + st)));
     }
-  }, 500)
-});
-    //setTimeout(function () {
-    //  var sec1 = angular.element('#sec-1');
-    //  sec1.inViewport(function () {
-    //    sec1.removeClass('animated bounce');
-    //    sec1.addClass('animated bounce');
-    //    //sec1.html ='seee';
-    //    //sec1.hide();
-    //    //sec1.show();
-    //
-    //
-    //  });
-    //}, 3000);
+
+    var secs = angular.element('[data=sec]'),
+        text = angular.element('[data=text]');
+    //var sec2 = angular.element('#sec-2');
+    angular.element(window).scroll(function() {
+        secs.each(function (i) {
+            var sec = angular.element(this);
+            if (checkVisible(sec, 'past')) {
+                sec.addClass( (i%2) ? 'flipInY' : 'flipInX' );
+            }
+        });
+        text.each(function (i) {
+            var txt = angular.element(this);
+            if (checkVisible(txt, 'past')) {
+                txt.addClass( 'zoomInUp' );
+            }
+        });
+    });
   });
