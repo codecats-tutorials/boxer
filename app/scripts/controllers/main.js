e931344160b12c24dcdddf22686231d0297d5a20
ssstrz <codecats@gmail.com>
2015-03-27 19:02:42 +0100

    front view, todo: animations on viewport

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,38 @@
  * Controller of the projApp
  */
 angular.module('projApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.ye = '12';
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $timeout) {
+
+function isScrolledIntoView(elem) {
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+$(window).on('DOMContentLoaded load resize scroll', function () {
+      var sec1 = $('#sec-1');
+  setInterval(function () {
+    console.log('a')
+    if (isScrolledIntoView('#sec-1')) {
+      alert('a')
+    }
+  }, 500)
+});
+    //setTimeout(function () {
+    //  var sec1 = angular.element('#sec-1');
+    //  sec1.inViewport(function () {
+    //    sec1.removeClass('animated bounce');
+    //    sec1.addClass('animated bounce');
+    //    //sec1.html ='seee';
+    //    //sec1.hide();
+    //    //sec1.show();
+    //
+    //
+    //  });
+    //}, 3000);
   });
